727118cb881158f795738fd110f4e2303786e0fa
FX <2601754903@qq.com>
2019-04-30 10:05:42 +0800

    新闻接口修改

--- parentjournalism/getlist.php
+++ parentjournalism/getlist.php
@@ -1,13 +1,6 @@
 <?php
 //	header("Content-type:text/html;charset=utf-8");
-	header("Content-type: application/json");
-	//跨域
-	header("Access-Control-Allow-Credentials: true");
-	header("Access-Control-Allow-Origin: *");
-	//CORS
-	header("Access-Control-Request-Methods:GET, POST, PUT, DELETE, OPTIONS");
-	header('Access-Control-Allow-Headers:x-requested-with,content-type,test-token,test-sessid');//注意头部自定义参数不要用下划线
-	$conn = new MySQLi("127.0.0.1","root","root","education");
+	include('./parentfunction.php');
 	$status=null;  //分类
 	$pageindex=0;  //页数
 	$pagesize=10;  //条数
